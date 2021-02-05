5c6cb8dedac634844629770fbc08c774cba6c2dd
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2021-02-06 03:16:57 +0700

    Coral Island Kickstarter Discord Bot - init project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const fetch = require('node-fetch');
 
 bot.login(TOKEN);
 
@@ -10,9 +11,15 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+  if (msg.content === 'ks-stat') {
+    fetch('https://www.kickstarter.com/projects/coralisland/coral-island-reimagining-the-farm-sim-game/stats.json?v=1')
+      .then(res => res.json())
+      .then(body => {
+        msg.channel.send(`Coral Island Kickstarter Status`)
+        msg.channel.send(`Total Pledge : US$${body.project.pledged}`)
+        msg.channel.send(`Total Backers : ${body.project.backers_count}`)
+        msg.channel.send(`Status : ${body.project.state}`)
+      });
 
   } else if (msg.content.startsWith('!kick')) {
     if (msg.mentions.users.size) {
