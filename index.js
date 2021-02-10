375885d1b74931fb26ebf2ab6c405b45e4db34b1
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2021-02-11 00:12:54 +0700

    add nextGoalAmmount and add msg.content trigger

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ci-help') {
+  if (msg.content === 'cihelp' || msg.content === 'Cihelp' || msg.content === 'ci help' || msg.content === 'Ci help') {
     msg.channel.send(`COMMAND LIST
-ci-social : Social Media of Coral Island Information
-ci-ks : Coral Island's Kickstarter Status
+ci social : Social Media of Coral Island Information
+ci ks : Coral Island's Kickstarter Status
     `)
   }
 
-  if (msg.content === 'ci-social') {
+  if (msg.content === 'cisocial' || msg.content === 'Cisocial' || msg.content === 'ci social' || msg.content === 'Ci social') {
     msg.channel.send(`Follow Us On
 Steam : https://store.steampowered.com/app/1158160/Coral_Island/
 Twitter : https://twitter.com/coralislandgame
 Reddit : https://www.reddit.com/r/coralisland/`)
   }
 
-  if (msg.content === 'ci-ks') {
+  if (msg.content === 'ci ks' || msg.content === 'Ci ks' || msg.content === 'CI KS') {
     fetch('https://www.kickstarter.com/projects/coralisland/coral-island-reimagining-the-farm-sim-game/stats.json?v=1')
       .then(res => res.json())
       .then(body => {
@@ -43,51 +43,64 @@ Reddit : https://www.reddit.com/r/coralisland/`)
         }
 
         const pledged = parseFloat(body.project.pledged)
-        let nextGoal, achievedGoal = ""
+        let nextGoalAmmount, nextGoal, achievedGoal
 
         if (pledged >= 70000) {
+          nextGoalAmmount = 125000
           achievedGoal = goals.goal0
           nextGoal = goals.goal1
         }
         if (pledged >= 125000) {
+          nextGoalAmmount = 175000
           achievedGoal = goals.goal1
           nextGoal = goals.goal2
         }
         if (pledged >= 175000) {
+          nextGoalAmmount = 200000
           achievedGoal = goals.goal2
           nextGoal = goals.goal3
         }
         if (pledged >= 200000) {
+          nextGoalAmmount = 250000
           achievedGoal = goals.goal3
           nextGoal = goals.goal4
         }
         if (pledged >= 250000) {
+          nextGoalAmmount = 300000
           achievedGoal = goals.goal4
           nextGoal = goals.goal5
         }
         if (pledged >= 300000) {
+          nextGoalAmmount = 35000
           achievedGoal = goals.goal5
           nextGoal = goals.goal6
         }
         if (pledged >= 350000) {
+          nextGoalAmmount = 400000
           achievedGoal = goals.goal6
           nextGoal = goals.goal7
         }
         if (pledged >= 400000) {
+          nextGoalAmmount = 500000
           achievedGoal = goals.goal7
           nextGoal = goals.goal8
         }
         if (pledged >= 500000) {
+          nextGoalAmmount = 600000
           achievedGoal = goals.goal8
           nextGoal = goals.goal9
+        }
+        if (pledged >= 600000) {
+          achievedGoal = goals.goal9
+          nextGoal = "All stretch goal completed"
         }
 
         msg.channel.send(`Coral Island Kickstarter Status
 Status : ${body.project.state}
-Total Pledge : US$${body.project.pledged}
+Total Pledge : US$${pledged}
 Total Backers : ${body.project.backers_count}
-Achieved Stretch Goal : ${achievedGoal}
-Next Stretch Goal : ${nextGoal}`)
+Previous Achieved Stretch Goal : ${achievedGoal}
+Next Stretch Goal : ${nextGoal} ($${nextGoalAmmount})`)
       });
   }
 });
